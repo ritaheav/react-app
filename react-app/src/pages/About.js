fb152785761e4479570e6e31888a7e4ece6e50bf
Ritaheav <rita20071995@hotmail.com>
2020-07-15 15:20:03 +0300

    add files

---
+++ react-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Информация</h1>
+                <p classNmae="lead">Версия приложения 1.0</p>
+            </div>
+        </div>
+    )
+}
