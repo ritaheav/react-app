b96b1d22eadac2fee67b7213845564fe7f481b77
Ritaheav <rita20071995@hotmail.com>
2020-07-26 22:19:31 +0300

    finish project

--- react-app/src/context/github/githubState.js
+++ react-app/src/context/github/githubState.js
@@ -1,7 +1,15 @@
 import React, {useReducer} from 'react';
+import axios from 'axios';
 import { GithubContext } from './githubContext';
 import { githubReducer } from './githubReducer';
 import { SEARCH_USERS, GET_USER, GET_REPOS, CLEAR_USERS, SET_LOADING } from '../Types';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+const withCreds = url => {
+    return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -15,27 +23,39 @@ export const GithubState = ({children}) => {
     const search = async value => {
         setLoading()
 
+        const response = await axios.get(
+            withCreds(`https://api.github.com/search/users?q=${value}&`)
+        )
+
         dispatch({
             type: SEARCH_USERS,
-            payload: []
+            payload: response.data.items  
         })
     }
 
     const getUser = async name => {
         setLoading()
 
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}?`)
+        )
+
         dispatch({
             type: GET_USER,
-            payload: {}
+            payload: response.data
         })
     }
 
     const getRepos = async name => {
         setLoading()
 
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+        )
+
         dispatch({
             type: GET_REPOS,
-            payload: []
+            payload: response.data
         })
     }
 
@@ -51,7 +71,8 @@ export const GithubState = ({children}) => {
 
     return (
         <GithubContext.Provider value={{
-            setLoading, search, getUser, getRepos, clearUsers
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
         }}>
             {children}
         </GithubContext.Provider>
--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -5,7 +5,7 @@ export const About = () => {
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">Информация</h1>
-                <p classNmae="lead">Версия приложения 1.0</p>
+                <p className="lead">Версия приложения 1.0</p>
             </div>
         </div>
     )
